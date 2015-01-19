ec77c26be102c7f7ad23b0b5a434ef69d18bcce0
Johan Vlaar <johan_vlaartje@hotmail.com>
2015-01-19 15:05:03 +0100

    navbar begin

---
+++ app/views/includes/core/header.blade.php
@@ -0,0 +1,42 @@
+<!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">part 1</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">part 2</a>
+                    </li>
+                    <li class="dropdown" id="menuLogin">
+                        <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Login</a>
+                        <div class="dropdown-menu" style="padding:17px;">
+                            <form class="form" id="formLogin"> 
+                                <input name="gebruikersnaam" type="text" placeholder="gebruikersnaam"> 
+                                <input name="wachtwoord"  type="password" placeholder="wachtwoord"><br>
+                                <button type="button" id="btnLogin" class="btn">Login</button>
+                            </form>
+                      </div>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
---
+++ public/assest/config.php
@@ -0,0 +1,4 @@
+<?php
+$name		= "De site name";
+
+?>
