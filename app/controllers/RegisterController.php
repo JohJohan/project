c785551a418a1cad3625194752aedf7b5f0ae2c2
Berwout Kruit <tehber@hotmail.com>
2015-01-20 11:16:42 +0100

    Registratie controller gemaakt en test pagina opgeschoont

---
+++ app/controllers/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+	Mew :3
+*/
+
+class RegisterController extends BaseController {
+	
+	public function register(){
+		//
+		$username 		= Input::get('gebruikersnaam');
+		$password 		= Input::get('wachtwoord');
+		$emailaddress 	= Input::get('emailaddress');
+		$role 			= 0;
+
+		$validator = Validator::make(Input::all(),array(
+				'gebruikersnaam' => 'required|max:155|unique:users',
+				'wachtwoord' => 'required|mix:4',
+				'emailaddress' => 'required|email|unique:users'
+			));
+
+		if ($validator->fails()){
+			//
+			Redirect::to('/register')->withErrors($validator);
+		}
+	}
+}
--- app/views/test.blade.php
+++ app/views/test.blade.php
@@ -4,12 +4,5 @@
 	</head>
 
 	<body>
-		@include('includes.error')
-
-		<form method="POST" action={{ URL::to('/login') }}>
-			<input type="text" name="gebruikersnaam">
-			<input type="password" name="wachtwoord">
-			<input type="submit" value="Log in">
-		</form>
 	</body>
 </html>
