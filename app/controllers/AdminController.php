ca25f05d971a9e07ffe3f7cca7d3a549344cbedb
Berwout Kruit <tehber@hotmail.com>
2015-01-23 13:49:35 +0100

    Basis admin panel staat en werkt.

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -9,6 +9,19 @@ class AdminController extends BaseController {
 
 	public function AdminCheck(){
 		//
-		
-	}
+		if (Auth::check()){
+			//
+			if (Auth::user()->role > 10){
+				//
+				//If the user has admin powers.
+				return View::make('admin.home');
+			} else {
+				//
+				return Redirect::to('/');
+			}
+		} else {
+			//
+			return Redirect::to('/');
+		}
+	}//End of AdminCheck
 }
--- app/views/admin/home.blade.php
+++ app/views/admin/home.blade.php
@@ -1,1 +1,50 @@
-Hoi
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>{{Config::get('app.sitenaam')}} - ADMIN PANEL</title>
+		<link rel="stylesheet" type="text/css" href="assest/css/admin/main.css">
+		<script type="text/javascript" src="assest/js/jquery.js"></script>
+		<script type="text/javascript" src="assest/js/admin/ControlPanel.js"></script>
+	</head>
+
+	<body>
+		<div id="navbar">
+			Control panel
+			<div class="button" onclick="openMe(this);">
+				Users
+				<a href="?action=1">
+					<div class="sub-button">
+						Profile
+					</div>
+				</a>
+
+				<a href="?action=2">
+					<div class="sub-button">
+						Ban
+					</div>
+				</a>
+			</div>
+
+			<div class="button" onclick="openMe(this);">
+				Roles
+				<a href="?action=3">
+					<div class="sub-button">
+						Set Role
+					</div>
+				</a>
+
+				<a href="?action=4">
+					<div class="sub-button">
+						Role Power
+					</div>
+				</a>
+
+				<a href="?action=5">
+					<div class="sub-button">
+						Remove Role
+					</div>
+				</a>
+			</div>
+		</div>
+	</body>
+</html>
